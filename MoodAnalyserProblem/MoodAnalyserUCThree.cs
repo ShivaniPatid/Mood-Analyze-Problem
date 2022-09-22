261d766dd4f10987b836a9a39faa2c8d9e1604a9
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-22 07:27:06 +0530

    [Shivani] UC3 - Custom Exception, Inform user if entered invalid mood

---
+++ MoodAnalyserProblem/MoodAnalyserUCThree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserUCThree
+    {
+        public string message;
+        public MoodAnalyserUCThree(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (message == null)
+                {
+                    throw (new MoodAnalysisException("Null Mood"));
+                }
+                else
+                {
+                    throw (new MoodAnalysisException("Empty Mood"));
+                }
+            }
+            catch (MoodAnalysisException ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+
+
+    public class MoodAnalysisException : Exception
+    {
+        public MoodAnalysisException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
+
