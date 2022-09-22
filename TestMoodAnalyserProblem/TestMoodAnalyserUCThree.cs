f8d0b34d9bd185aa73547921ff144ad8b77cfa36
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-22 07:29:50 +0530

    [Shivani] Test cases for UC3

---
+++ TestMoodAnalyserProblem/TestMoodAnalyserUCThree.cs
@@ -0,0 +1,35 @@
+﻿using MoodAnalyserProblem;
+
+namespace TestMoodAnalyser
+{
+    [TestClass]
+    public class TestMoodAnalyserUCThree
+    {
+
+        [TestMethod]
+        public void GivenMessageUCThreeTCOne()
+        {
+            string msg = null;
+            string expected = "Null Mood";
+
+            MoodAnalyserUCThree moodAnalyserUCThree = new MoodAnalyserUCThree(msg);
+            string actual = moodAnalyserUCThree.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+            Console.WriteLine(actual);
+        }
+
+        [TestMethod]
+        public void GivenMessageUCThreeTCTwo()
+        {
+            string msg = "";
+            string expected = "Empty Mood";
+
+            MoodAnalyserUCThree moodAnalyserUCThree = new MoodAnalyserUCThree(msg);
+            string actual = moodAnalyserUCThree.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+            Console.WriteLine(actual);
+        }
+    }
+}
