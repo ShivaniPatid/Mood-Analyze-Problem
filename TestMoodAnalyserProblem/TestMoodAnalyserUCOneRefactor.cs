9f6622df52e5e958e3f92bde676f4e67285bca40
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-22 07:07:42 +0530

    [Shivani] Test cases for UC1 - Refactor

---
+++ TestMoodAnalyserProblem/TestMoodAnalyserUCOneRefactor.cs
@@ -0,0 +1,37 @@
+﻿using MoodAnalyserProblem;
+
+namespace TestMoodAnalyser
+{
+    [TestClass]
+    public class TestMoodAnalyserUCOneRefactor
+    {
+
+        [TestMethod]
+        public void GivenMessageUCOneTCOneRepeat()
+        {
+            string msg = "I am in Sad Mood";
+            string expected = "sad";
+
+            MoodAnalyserUCOneRefactor moodAnalyserUCOne = new MoodAnalyserUCOneRefactor(msg);
+            string actual = moodAnalyserUCOne.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+            Console.WriteLine(actual);
+        }
+
+        [TestMethod]
+        public void GivenMessageUCOneTCTwoRepeat()
+        {
+            string msg = "I am in Happy Mood";
+            string expected = "happy";
+
+            MoodAnalyserUCOneRefactor moodAnalyserUCOne = new MoodAnalyserUCOneRefactor(msg);
+            string actual = moodAnalyserUCOne.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+            Console.WriteLine(actual);
+        }
+
+
+    }
+}
