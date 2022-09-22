e37fba471966ab232f79f35822fb6ef2b9afa52e
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-22 07:02:34 +0530

    [Shivani] UC1 - Refactor - Given message sad or happy

---
+++ MoodAnalyserProblem/MoodAnalyserUCOneRefactor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserUCOneRefactor
+    {
+        public string message;
+
+        public MoodAnalyserUCOneRefactor(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            message = message.ToLower();
+            if (message.Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
