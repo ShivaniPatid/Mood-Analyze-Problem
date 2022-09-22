7e1177f00e1df03d0aa9b8a82597da7c527c5d97
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-22 07:17:20 +0530

    [Shivani] Test cases for UC2

---
+++ TestMoodAnalyserProblem/TestMoodAnalyserUCTwo.cs
@@ -0,0 +1,23 @@
+﻿using MoodAnalyserProblem;
+
+namespace TestMoodAnalyser
+{
+    [TestClass]
+    public class TestMoodAnalyserUCTwo
+    {
+
+        [TestMethod]
+        public void GivenMessageUCTwoTCOne()
+        {
+            string msg = null;
+            string expected = "happy";
+
+            MoodAnalyserUCTwo moodAnalyserUCTwo = new MoodAnalyserUCTwo(msg);
+            string actual = moodAnalyserUCTwo.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+            Console.WriteLine(actual);
+        }
+    }
+
+}
