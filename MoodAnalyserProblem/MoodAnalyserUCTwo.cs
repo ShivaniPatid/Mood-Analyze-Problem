99be7e38d969dc676506f6928eaeb6baac61c80f
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-22 07:12:51 +0530

    [Shivani] UC2 - Handle Exception if User Provides Invalid Mood

---
+++ MoodAnalyserProblem/MoodAnalyserUCTwo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserUCTwo
+    {
+        public string message;
+
+        public MoodAnalyserUCTwo(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                message = message.ToLower();
+                if (message.Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "happy";
+            }
+        }
+    }
+}
