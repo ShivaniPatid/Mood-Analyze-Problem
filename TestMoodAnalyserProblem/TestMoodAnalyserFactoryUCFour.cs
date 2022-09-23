8afb92e545135c8913fec656bdefd6fce3f92577
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-23 18:11:37 +0530

    [Shivani] Test cases for UC4

---
+++ TestMoodAnalyserProblem/TestMoodAnalyserFactoryUCFour.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using MoodAnalyserProblem;
+
+namespace TestMoodAnalyserProblem
+{
+    [TestClass]
+    public class TestMoodAnalyserUCFour
+    {
+        [TestMethod]
+        public void GivenMoodAnalyserClassName_ShouldReturnMoodAnalyserObject()
+        {
+            
+            object expected = new MoodAnalyserUCOne();
+            object obj = MoodAnalyserFactoryUCFour.CreateMoodAnalyse("MoodAnalyserProblem.MoodAnalyserUCOne", "MoodAnalyserUCOne");
+            expected.Equals(obj);
+        }
+        
+    }
+
+
+}
