9a22cc084488f9d4ffca2ff28cd2f0f117f5cf75
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-23 18:54:27 +0530

    [Shivani] Test cases for UC5

---
+++ TestMoodAnalyserProblem/TestMoodAnalyserFactoryUCFive.cs
@@ -0,0 +1,20 @@
+﻿using MoodAnalyserProblem;
+
+namespace TestMoodAnalyserProblem
+{
+    [TestClass]
+    public class TestMoodAnalyserUCFive
+    {
+        [TestMethod]
+        public void GivenMoodAnalyserClassName()
+        {
+
+            object expected = new MoodAnalyser("HAPPY");
+            object obj = MoodAnalyserFactoryUCFive.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserProblem.MoodAnalyser", "MoodAnalyser","HAPPY");
+            expected.Equals(obj);
+        }
+
+    }
+
+
+}
