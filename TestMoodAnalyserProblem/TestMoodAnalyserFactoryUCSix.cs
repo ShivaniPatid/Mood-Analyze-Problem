57e8852c13882327e85efdc8cdc56e4f49e6840c
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-23 19:18:13 +0530

    [Shivani] Test cases for UC6

---
+++ TestMoodAnalyserProblem/TestMoodAnalyserFactoryUCSix.cs
@@ -0,0 +1,21 @@
+﻿using MoodAnalyserProblem;
+
+namespace TestMoodAnalyserProblem
+{
+    [TestClass]
+    public class TestMoodAnalyserUCSix
+    {
+        [TestMethod]
+        public void GivenHappyMoodShouldReturnHappy()
+        {
+
+            object expected ="HAPPY";
+            string mood = MoodAnalyserFactoryUCSix.InvokeAnalyseMood("HAPPY", "AnalyseMood");
+            Assert.AreEqual(expected, mood);
+            Console.WriteLine(mood);
+        }
+
+    }
+
+
+}
